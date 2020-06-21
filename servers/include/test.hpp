a20aa6550dbd3c7a4333dc6565ed3b773330487d
Drewol <emildraws@gmail.com>
2020-06-21 15:53:40 +0200

    Added more examples to test

--- servers/include/test.hpp
+++ servers/include/test.hpp
@@ -2,14 +2,20 @@
 #include "BaseServer.hpp"
 using namespace test;
 using namespace grpc;
-class TestImpl final : public TestService::Service, public BaseServer {
+class TestImpl final : public TestService::Service, public BaseServer
+{
 public:
-    TestImpl(struct NVGcontext* vg);
-    Status SetTab(ServerContext* context, const ChangeTab* tab, EmptyResult* response) override;
-    Status GetTab(ServerContext* context, const EmptyResult* tab, ChangeTab* response) override;
-    void Render(struct NVGcontext* vg, float deltaTime) override;
+    TestImpl(struct NVGcontext *vg);
+    Status SetTab(ServerContext *context, const ChangeTab *tab, EmptyResult *response) override;
+    Status GetTab(ServerContext *context, const EmptyResult *tab, ChangeTab *response) override;
+    Status SetMessage(ServerContext *context, const StringRequest *request, EmptyResult *response) override;
+    Status SetHue(ServerContext *context, const HueRequest *hue, EmptyResult *response) override;
+    void Render(struct NVGcontext *vg, float deltaTime) override;
+
 private:
     test::TabIndex currentTab;
     int font;
     float avgFps = 0.016f;
+    double hue = 0.0f;
+    std::string message;
 };
--- servers/src/test.cpp
+++ servers/src/test.cpp
@@ -5,50 +5,68 @@
 using namespace test;
 using namespace grpc;
 
-TestImpl::TestImpl(NVGcontext* vg)
+TestImpl::TestImpl(NVGcontext *vg)
 {
     font = nvgCreateFont(vg, "default", "fonts/Roboto/Roboto-Regular.ttf");
     currentTab = TabIndex::HOME;
+    message = "";
 }
 
-Status TestImpl::SetTab(ServerContext* context, const ChangeTab* tab, EmptyResult* response) {
+Status TestImpl::SetTab(ServerContext *context, const ChangeTab *tab, EmptyResult *response)
+{
     printf("Changing %d->%d\n", currentTab, tab->tab());
     currentTab = tab->tab();
     return Status::OK;
 }
 
-void TestImpl::Render(NVGcontext* vg, float deltaTime)
+Status TestImpl::SetMessage(ServerContext *context, const StringRequest *request, EmptyResult *response)
 {
-    std::string fpsText;
+    message = request->text();
+    return Status::OK;
+}
+
+Status TestImpl::GetTab(ServerContext *context, const EmptyResult *tab, ChangeTab *response)
+{
+    response->set_tab(currentTab);
+    return Status::OK;
+}
+
+Status TestImpl::SetHue(ServerContext *context, const HueRequest *hue, EmptyResult *response)
+{
+    this->hue = hue->hue();
+    printf("Changing hue to %.2f\n", this->hue);
+    return Status::OK;
+}
+
+void TestImpl::Render(NVGcontext *vg, float deltaTime)
+{
+    std::string text, title;
     char buff[64];
     avgFps = avgFps * 0.98f + deltaTime * 0.02f;
-    snprintf(buff , 32, "%.2f FPS", 1.0 / avgFps);
-    fpsText = buff;
+    snprintf(buff, 32, "%.2f FPS", 1.0 / avgFps);
     nvgBeginPath(vg);
-    nvgFillColor(vg, nvgRGB(255,255,255));
+    nvgFillColor(vg, nvgHSL(static_cast<float>(hue), 1.0f, 0.5f));
     nvgFontFaceId(vg, font);
-    nvgFontSize(vg, 200);
+    nvgFontSize(vg, 40);
     nvgTextAlign(vg, NVG_ALIGN_TOP | NVG_ALIGN_LEFT);
-    nvgText(vg, 50, 50, fpsText.c_str(), nullptr);
     switch (currentTab)
     {
     case TabIndex::HOME:
-        fpsText = "Tab: Home";
+        title = "Tab: Home";
+        text = "Welcome to PiVis!";
         break;
-        case TabIndex::DATA:
-        fpsText = "Tab: Data";
+    case TabIndex::DATA:
+        title = "Tab: Data";
+        text = message;
         break;
-        case TabIndex::PERF:
-        fpsText = "Tab: Performace";
+    case TabIndex::PERF:
+        title = "Tab: Performace";
+        text = buff;
         break;
-    
+
     default:
         break;
     }
-    nvgText(vg, 50, 300, fpsText.c_str(), nullptr);
-}
-
-Status TestImpl::GetTab(ServerContext* context, const EmptyResult* tab, ChangeTab* response) {
-    response->set_tab(currentTab);
-    return Status::OK;
+    nvgText(vg, 50, 10, title.c_str(), nullptr);
+    nvgText(vg, 50, 40, text.c_str(), nullptr);
 }
